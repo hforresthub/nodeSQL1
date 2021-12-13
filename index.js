f505e2cf1773423ade024b3accade4da53298123
Hal <hgrforrest@gmail.com>
2021-12-13 14:43:40 -0500

    created a database!

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ const con = mysql.createConnection({
 	password: ""
 })
 
-con.connect(function (err) {
+con.connect((err) => {
 	if (err) {
 		throw err
 	}
 	console.log("Connected!")
+	const sql = "CREATE DATABASE mydb"
+	con.query(sql, (err, res) => {
+		if (err) {
+			throw err
+		}
+		console.log("Result: " + res)
+	})
 })
